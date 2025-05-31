10a8a0710f393ef64c939761ac574163c8ea0bd6
Sahas Timilsina <tsahas2059@gmail.com>
2025-05-31 09:03:35 +0545

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sahas001/container
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func run() {
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error running the /proc/self/exe command", err)
+		os.Exit(1)
+	}
+}
+
+func child() {
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+
+	// set hostname of the new UTS namespace
+	if err := syscall.Sethostname([]byte("HMcontainer")); err != nil {
+		fmt.Println("Error setting hostname:", err)
+		os.Exit(1)
+	}
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error running the child command in the new namespace:", err)
+		os.Exit(1)
+	}
+
+}
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		fmt.Println("Usage: go run main.go run <command> [args] | child <command> [args]")
+		os.Exit(1)
+	}
+}
